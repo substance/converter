640857ac82208194345b179f349037710e867527
vectorsize <vectorsize@gmail.com>
2013-05-25 23:54:46 +0200

    implemented headings properly, polished some stuff and prepared for future node types

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var https  = require('https');
-var http  = require('http');
-var _ = require('underscore');
-// var Github = require('./lib/github');
-var Document = require('./lib/document/document');
-var util = require('./lib/util/util');
 
-// Should output substance doc
-// function loadMarkdown(username, repo, branch, path, cb) {
-//   var github = new Github({});
-//   var repo = github.getRepo(username, repo);
-//   repo.read(branch, path, cb);
-// }
+var express = require('express'),
+    fs = require('fs'),
+    app = express(),
+    _ = require('underscore'),
+    urlparser = require("url"),
+    Document = require('./lib/document/document'),
+    util = require('./lib/util/util');
 
 // Convert pandoc JSON output into substanc format
 // Should output substance doc
@@ -28,38 +20,61 @@ function convert(pandocAST, cb) {
   var doc = new Document({id: "a_new_doc"});
   var elements = pandocAST[1];
 
+  // parses text objects into strings
   function getText(pieces) {
     var res = "";
     _.each(pieces, function(piece) {
+
       if (piece === "Space") {
         res += " ";
+      } else if (piece["Emph"]) {
+        // Emphasis object
+        // todo: implement properly
+        res += getText(piece["Emph"]);
+      } else if (piece["Strong"]) {
+        // Strong object
+        // todo: implement properly
+        res += getText(piece["Strong"]);
+      } else if (piece["Link"]) {
+        // link object
+        // todo: implement properly
+        res += piece["Link"][1][0];
       } else if (piece["Str"]) {
         // propably a Str object
         res += piece["Str"];
       }
     });
-    console.log('res', res);
+    
     return res;
   }
 
+  // we loop through each converted element
   _.each(elements, function(elem) {
+
     // inspect element
-    var nodeType = _.first(Object.keys(elem));
+    if(typeof elem === 'object'){
+        var nodeType = _.first(Object.keys(elem));
+    } else {
+      // horizontalrule doesnt come as an object
+      // todo cover all the cases
+      var nodeType = elem;
+    }
 
+    // Headings
     if (nodeType === 'Header') {
-      // Extract plain text for heading
-
-      // Insert a new 
+      // Insert a new heading
       doc.apply(["insert", {
         "id": "heading:"+getId(),
         "type": "heading",
         "target": "back",
         "data": {
-          "content": getText(elem[nodeType][2])
+          "level": elem[nodeType][0],
+          "content": getText(elem[nodeType][1])
         }
       }]);
     }
 
+    // Text
     if (nodeType === 'Para') {
       // Insert a new text node
       doc.apply(["insert", {
@@ -73,8 +88,32 @@ function convert(pandocAST, cb) {
     }
   });
 
-
   cb(null, doc.toJSON());
+}
+
+function getFile(url, cb) {
+  var parts = urlparser.parse(url),
+      protocol;
+
+  // quick workaround
+  // can be done better?
+  if (parts.protocol === 'http:'){
+    protocol = require('http');
+  } else if(parts.protocol === 'https:') {
+    protocol = require('https');
+  }
+
+  protocol.get(url, function(res) {
+    var result = "";
+    res.on('data', function ( d ) {
+      result += d.toString();
+    });
+    res.on('end', function() {
+      cb(null, result);
+    });
+  }).on('error', function( e ) {
+    cb(err);
+  });
 }
 
 function toPandoc(url, cb) {
@@ -95,34 +134,19 @@ function toPandoc(url, cb) {
 }
 
 
-
-function getFile(url, cb) {
-  http.get(url, function(res) {
-    var result = "";
-    res.on('data', function ( d ) {
-      result += d.toString();
-    });
-    res.on('end', function() {
-      cb(null, result);
-    });
-  }).on('error', function( e ) {
-    cb(err);
-  });
-}
-
-
-
 // Convert to SUBSTANCE
 // -------------
 
 app.get('/', function(req, res) {
-  toPandoc('http://ma.zive.at/hello.md', function(err, pandocAST) {
+  var url = 'http://bywordapp.com/markdown/guide.md';
+  // _mql: this works perfectly for me! you should fix your issues ;)
+  // var url = 'https://raw.github.com/dtao/lazy.js/master/README.md';
+
+  toPandoc(url, function(err, pandocAST) {
     convert(pandocAST, function(err, substanceDoc) {
       res.jsonp(substanceDoc);
     });
   });
 });
-
-
 
 app.listen(process.env.PORT || 5001);
