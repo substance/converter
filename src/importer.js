e289ba65d8a2840a221cc36818cacb40c6e5ed0e
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-08-13 00:32:50 +0200

    Basic importing of headings and paragraphs.

--- src/importer.js
+++ src/importer.js
@@ -1,11 +1,17 @@
 "use strict";
 
+var util = require("substance-util");
+var errors = util.errors;
+var Article = require("substance-article");
+
+var ImporterError = errors.define("ImporterError");
+
 var Importer = function() {
 };
 
 Importer.Prototype = function() {
 
-  this.import = function(pandocJSON) {
+  this.import = function(input) {
     var state = {};
 
     // an id generator for different types
@@ -16,24 +22,133 @@ Importer.Prototype = function() {
       return type +"_"+ids[type];
     };
 
-    state.input = pandocJSON;
+    state.input = input;
 
-    var nodes;
-
-    return this.document(nodes, state);
+    return this.document(state, input);
   };
 
-  this.document = function(nodes, state) {
+  this.document = function(state, input) {
+    var meta = input[0];
+    var doc = new Article({"id": meta.doc_id});
+
+    state.doc = doc;
+
+    // all nodes on this level are inserted and shown
+    var nodes = input[1];
+    for (var i = 0; i < nodes.length; i++) {
+      var node = this.topLevelNode(state, nodes[i]);
+      doc.show("content", node.id, i);
+    }
+
+    return doc;
   };
 
-  this.paragraph = function(paragraphNode, state) {
+  this.topLevelNode = function(state, input) {
+    var type = Object.keys(input)[0];
+
+    switch(type) {
+      case "Header":
+        return this.header(state, input["Header"]);
+      case "Para":
+        return this.paragraph(state, input["Para"]);
+      default:
+        throw new ImporterError("Node not supported: "+type);
+    }
+
   };
 
-  this.text = function(textNode, state) {
+  /*
+  Example:
+    {
+        "Header": [
+            1,
+            [
+                "heading",
+                [],
+                []
+            ],
+            [
+                {
+                    "Str": "Heading"
+                }
+            ]
+        ]
+    },
+  */
+  this.header = function(state, input) {
+    var doc = state.doc;
+
+    var level = input[0];
+    var content = this.text(state, input[2]);
+
+    var id = state.nextId("header");
+    var node = {
+      id: id,
+      type: "heading",
+      level: level,
+      content: content
+    };
+    return doc.create(node);
+  };
+
+  /*
+  Example:
+  {
+      "Para": [
+          {
+              "Str": "And"
+          },
+          "Space",
+          {
+              "Str": "a"
+          },
+          "Space",
+          {
+              "Str": "paragraph"
+          }
+      ]
+  }
+  */
+  this.paragraph = function(state, input) {
+    var doc = state.doc;
+    var content = this.text(state, input);
+
+    var id = state.nextId("paragraph");
+    var node = {
+      id: id,
+      type: "paragraph",
+      content: content
+    };
+    return doc.create(node);
+  };
+
+  // Retrieves a text block from an array of textish fragments
+  // and creates annotations on the fly.
+  // --------
+  //
+
+  this.text = function(state, textFragments) {
+    var result = [];
+    for (var i = 0; i < textFragments.length; i++) {
+      var item = textFragments[i];
+
+      if (item === "Space") {
+        result.push(" ");
+      }
+      else if (item["Str"] !== undefined) {
+        result.push(item["Str"]);
+      }
+      else {
+        throw new ImporterError("Unsupported fragment for textish: " + item);
+      }
+    }
+
+    return result.join("");
   };
 
 };
 
 Importer.prototype = new Importer.Prototype();
+Importer.ImporterError = ImporterError;
 
 module.exports = Importer;
--- tests/importer_test.js
+++ tests/importer_test.js
@@ -14,13 +14,24 @@ var Importer = require('../src/importer');
 var ImporterTest = function () {
 
   this.setup = function() {
-
+    this.importer = new Importer();
   };
 
   this.actions = [
 
-    "Start over", function() {
-      
+    "Heading and Paragraph", function() {
+      var input = require("../data/heading_and_paragraph.json");
+
+      var doc = this.importer.import(input);
+
+      var h1 = doc.get("header_1");
+      var p1 = doc.get("paragraph_1");
+
+      assert.isDefined(h1);
+      assert.isDefined(p1);
+
+      assert.isEqual("Heading", h1.content);
+      assert.isEqual("And a paragraph", p1.content);
     }
   ];
 };
