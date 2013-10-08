e10cb41e6fdbe2383e5ee4a62bb482bffb9d6080
Michael Aufreiter <michael@substance.io>
2013-10-08 18:26:15 +0200

    Cleanup.

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 "use strict";
 
-// var errors = require("./src/converter_errors");
-// var PandocImporter = require("./src/pandoc_importer");
-// var PandocExporter = require("./src/pandoc_exporter");
-// // currently not supported under node.js as we use the DOM for XML parsing
-// if (global.window) {
-//   var NLMImporter = require("./src/nlm_importer");
-// }
-
 module.exports = require("./src/converter");
-
-// module.exports = {
-//   ImporterError: errors.ImporterError,
-//   ExporterError: errors.ExporterError,
-//   PandocImporter: PandocImporter,
-//   PandocExporter: PandocExporter,
-//   NLMImporter: NLMImporter,
-// };
